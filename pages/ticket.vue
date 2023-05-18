d5a0d0d9028e389f93a348470014a3ae1839e933
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-18 17:47:26 +0700

    create page ticket

---
+++ pages/ticket.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "base",
+});
+</script>
+
+<template>
+  <section class="bg-white py-8">
+    <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+      <HeadStore />
+
+      <CardTicket v-for="n in 10" :key="n" />
+    </div>
+  </section>
+</template>
+
+<style></style>
