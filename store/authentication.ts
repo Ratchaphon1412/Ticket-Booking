bacc8ea296b38f2aa90fd0607e81fc708855bb82
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-22 19:13:55 +0700

    edit authentication store

--- store/authentication.ts
+++ store/authentication.ts
@@ -1,35 +1,56 @@
 import {defineStore} from 'pinia'
+import {ref,watch} from 'vue'
+
+// import { useLocalStorage } from '@vueuse/core'
 
 
 interface User{
     id: number;
-    token: string;
-    user:object|null;
+    token:string;
+    username:string;
     role: string;
     isAuth: boolean;
+    
 }
 
 export const useAuthenticationStore = defineStore('authenticationStore',{
+
     state: ():User=>({
         id: 0,
         token: '',
-        user:null,
-        role: '',
+        username:'',
+        role:'',
         isAuth: false,
     }),
     actions: {
-        async login(email:string,passoword:string):Promise<void>{
+        async login(email:string,passoword:string){
             const axios= useNuxtApp().$axios;
             
-            const {data} = await axios.post('/api/login',{
-                email,
-                passoword
+            const {data} = await axios.post('/api/auth/signin',{
+                username:email,
+                password:passoword
             })
-           
-            this.id = data.id;
-            this.token = data.token;
-            this.user = data.user;
-            this.role = data.role;
+       
+            this.isAuth = data.auth
+            this.token = data.accessToken
+            this.username = data.username
+            this.role = data.authorities[0]
+        },
+        async signup(name:string,username:string,email:string,password:string){
+
+            const axios= useNuxtApp().$axios;
+            const role =['USER'];
+            
+            const {data} = await axios.post('/api/auth/signup',{
+                name:name,
+                username:username,
+                email:email,
+                password:password,
+                roles:role
+            })
+            console.log(data);
+
+
         }
 
 
@@ -41,7 +62,13 @@ export const useAuthenticationStore = defineStore('authenticationStore',{
         getRole():string{
             return this.role;
         }
-    }
+    },
+    persist: {
+        storage: persistedState.sessionStorage,
+      },
+
 
 
 })
+
+
