412f4b89a12ca677f1405d7403453aefba9e4c99
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-22 19:14:44 +0700

    create page admin

---
+++ pages/admin.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+definePageMeta({
+  layout: "base",
+  middleware: ["authadmin"],
+});
+</script>
+
+<template>
+  <div>
+    <h1>Admin</h1>
+  </div>
+</template>
