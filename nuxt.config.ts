4796964ff59c52702aae05d8841da9fb668398a8
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-22 19:06:48 +0700

    add modules and base_url backend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-      '~/assets/css/main.css',
-    '@fortawesome/fontawesome-svg-core/styles.css'
+  css: [
+    '~/assets/css/main.css',
+  '@fortawesome/fontawesome-svg-core/styles.css'
+],
+
+  modules: [
+      '@pinia/nuxt',
+      '@pinia-plugin-persistedstate/nuxt',
   ],
-    modules: [
-        '@pinia/nuxt',
-        '@vueuse/nuxt',
-        '@pinia-plugin-persistedstate/nuxt',
-       
-    ],
+
+  plugins: [
+      '~/plugins/api.ts',
+  
     
-    plugins: [
-        '~/plugins/api.ts',
-      
-    ],
+  ],
 
-      postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-      runtimeConfig: {
-        public:{
-           baseURL: process.env.BASE_URL || 'http://localhost:3000',
-        }
-       },
-      
-   
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 
+  runtimeConfig: {
+    public:{
+       baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    }
+   },
+
+  
 })
