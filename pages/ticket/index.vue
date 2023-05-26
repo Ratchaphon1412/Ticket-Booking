8c31a8488f2f220f8c2b613024bf9294f745cafd
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-26 17:32:02 +0700

    add condition when search and not have items

--- pages/ticket/index.vue
+++ pages/ticket/index.vue
@@ -5,7 +5,7 @@ definePageMeta({
   layout: "base",
   middleware: ["authuser"],
 });
-const { getConcertData } = storeToRefs(conCertStore);
+const { getConcertData }: any = storeToRefs(conCertStore);
 
 onMounted(async () => {
   await conCertStore.getConCert();
@@ -16,14 +16,22 @@ onMounted(async () => {
   <section class="bg-white py-8">
     <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
       <HeadStore />
-      <CardTicket
-        v-for="n in getConcertData"
-        :key="n"
-        :title="n.name"
-        :image="n.image"
-        :status="n.status"
-        :id="n.id"
-      />
+      <div v-if="getConcertData.length > 0" class="flex flex-row w-full">
+        <CardTicket
+          v-for="n in getConcertData"
+          :key="n"
+          :title="n.name"
+          :image="n.image"
+          :status="n.status"
+          :id="n.id"
+        />
+      </div>
+      <div
+        v-else
+        class="w-screen h-screen flex flex-row justify-center justify-items-center items-center"
+      >
+        <h3 class="text-xl text-gray-500 font-bold">Not Found</h3>
+      </div>
     </div>
   </section>
 </template>
