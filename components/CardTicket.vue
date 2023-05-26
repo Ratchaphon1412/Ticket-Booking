6a3cf66280a27441f3328bc4586f149b757cd0f4
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-26 18:35:09 +0700

    add condition check status

--- components/CardTicket.vue
+++ components/CardTicket.vue
@@ -17,16 +17,23 @@ const routeString = `/ticket/details/${id}`;
 
 <template>
   <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-    <NuxtLink :to="routeString">
+    <NuxtLink :to="status != 'Buy Ticket' ? '' : routeString">
       <img
         class="hover:grow hover:shadow-lg h-96 w-72 object-cover object-center"
         :src="image"
       />
-      <div class="pt-3 flex items-center justify-between mb-8">
+      <div class="pt-3 flex items-center justify-between mb-3">
         <p class="font-bold">{{ title }}</p>
       </div>
 
       <button
+        v-if="status === 'Buy Ticket'"
+        class="bg-blue-500 hover:bg-blue-600 text-gray-800 font-bold py-2 px-4 rounded w-full"
+      >
+        {{ status }}
+      </button>
+      <button
+        v-else
         class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded w-full"
       >
         {{ status }}
