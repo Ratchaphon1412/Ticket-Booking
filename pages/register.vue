dcd11d9217ddeb89a3911a02b142645b69959e60
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-06-03 01:58:25 +0700

    require feild

--- pages/register.vue
+++ pages/register.vue
@@ -74,6 +74,7 @@ async function register(): Promise<void> {
                 v-model.trim="username"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="username"
+                required
               />
             </div>
             <div>
@@ -88,6 +89,7 @@ async function register(): Promise<void> {
                 v-model.trim="name"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="name"
+                required
               />
             </div>
             <div>
@@ -102,6 +104,7 @@ async function register(): Promise<void> {
                 v-model.trim="email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="name@company.com"
+                required
               />
             </div>
             <div>
@@ -115,6 +118,7 @@ async function register(): Promise<void> {
                 name="password"
                 v-model.trim="password"
                 placeholder="••••••••"
+                required
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
@@ -129,6 +133,7 @@ async function register(): Promise<void> {
                 name="password"
                 v-model.trim="confirmpPassword"
                 placeholder="••••••••"
+                required
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
